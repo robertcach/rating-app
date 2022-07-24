59b1a6e32badf2ecd10c41185d1e076fafcc4dae
Robert Cabrera <cabrera.rch@gmail.com>
2022-07-24 21:12:51 +0200

    feat: add styles properly

--- src/components/Repositories/RepositoryItem/RepositoryItem.jsx
+++ src/components/Repositories/RepositoryItem/RepositoryItem.jsx
@@ -1,9 +1,9 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const RepositoryItem = (props) => {
   return (
-    <View key={props.id} style={{ padding: 20, paddingBottom: 5, paddingTop: 5 }}>
-      <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>Id: {props.id}</Text>
+    <View key={props.id} style={styles.container}>
+      <Text style={styles.strong}>Id: {props.id}</Text>
       <Text>FullName: {props.fullName}</Text>
       <Text>Description:{props.description}</Text>
       <Text>Language: {props.language}</Text>
@@ -14,5 +14,18 @@ const RepositoryItem = (props) => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    paddingBottom: 5,
+    paddingTop: 5,
+  },
+  strong: {
+    color: 'blue',
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+});
 
 export default RepositoryItem;
