47c90d7b24361235332dd4c05239de7547587ea1
Robert Cabrera <cabrera.rch@gmail.com>
2022-07-24 19:46:29 +0200

    Party begins

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableWithoutFeedback onPress={() => Alert.alert("Hemos hecho clic en el texto")}>
+        <Text>Hola Mundo!</Text>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
