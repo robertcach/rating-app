58335f89e0841412710f0924644abfc62705efbf
Robert Cabrera <cabrera.rch@gmail.com>
2022-08-07 13:26:34 +0200

    Feat: link frontend with api and show data in app

--- src/components/Repositories/RepositoryList/RepositoryList.jsx
+++ src/components/Repositories/RepositoryList/RepositoryList.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import { Text, FlatList } from 'react-native';
-import repositories from '../../../data/repositories';
 import RepositoryItem from '../RepositoryItem/RepositoryItem';
 
 const RepositoryList = () => {
+  const [repositories, setRepositories] = useState(null);
+
+  const fetchRepositories = async () => {
+    const response = await fetch('http://192.168.1.130:5000/api/repositories');
+    const json = await response.json();
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  const repositoriesNodes = repositories ? repositories.edges.map((edge) => edge.node) : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoriesNodes}
       ItemSeparatorComponent={() => <Text> </Text>}
       renderItem={({ item: repo }) => <RepositoryItem {...repo} />}
     ></FlatList>
